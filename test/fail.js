29753fa4481eac79aaedc6b52261b3a25ee8b87f
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-14 21:48:31 -0300

    Add fail test

---
+++ test/fail.js
@@ -0,0 +1,42 @@
+'use strict';
+const pkgUtils = require('..');
+const expect = require('chai').expect;
+const path = require('path');
+
+describe('Check pkgUtils with package.json', () => {
+    describe('Check package.json', () => {
+
+        it('Check name', () => {
+            expect(pkgUtils.getProjectName('notfail')).to.be.undefined;
+        });
+
+        it('Check version', () => {
+            expect(pkgUtils.getVersion('notfail')).to.be.equal('0.0.0');
+        });
+
+        it('Check description', () => {
+            expect(pkgUtils.getDescription('notfail')).to.be.equal('');
+        });
+
+        it('Check author', () => {
+            expect(pkgUtils.getAuthor('notfail')).to.be.equal('');
+        });
+
+        it('Check email', () => {
+            expect(pkgUtils.getEmail('notfail')).to.be.equal('');
+        });
+
+        it('Check username', () => {
+            expect(pkgUtils.getUsername('notfail')).to.be.equal('');
+        });
+
+        it('Check license', () => {
+            expect(pkgUtils.getLicense('notfail')).to.be.equal('GPL-3.0');
+        });
+
+        it('Check appModule', () => {
+            expect(pkgUtils.getModuleName('notfail')).to.be.equal('appModule');
+        });
+
+    });
+});
